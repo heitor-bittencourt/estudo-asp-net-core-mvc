df05f2718b18143fa4676b60d3cddf55f261adbc
heitor-bittencourt <heitor.g.bittencourt@gmail.com>
2020-10-19 23:09:08 -0300

    CRUD Scaffolding

---
+++ WebVendasMvc/Data/WebVendasMvcContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace WebVendasMvc.Models
+{
+    public class WebVendasMvcContext : DbContext
+    {
+        public WebVendasMvcContext (DbContextOptions<WebVendasMvcContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<WebVendasMvc.Models.Departamento> Departamento { get; set; }
+    }
+}

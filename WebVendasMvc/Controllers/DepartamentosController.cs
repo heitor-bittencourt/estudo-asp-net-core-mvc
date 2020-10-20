676839813c75c10229086bddc7f7a7ceeb900570
heitor-bittencourt <heitor.g.bittencourt@gmail.com>
2020-10-19 22:58:35 -0300

    Primeiro teste MVC: Departamento

---
+++ WebVendasMvc/Controllers/DepartamentosController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebVendasMvc.Models;
+
+namespace WebVendasMvc.Controllers
+{
+    public class DepartamentosController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<Departamento> lista = new List<Departamento>();
+            lista.Add(new Departamento
+            {
+                Id = 1,
+                Nome = "Eletrônicos"
+            });
+
+            lista.Add(new Departamento
+            {
+                Id = 2,
+                Nome = "Fashion"
+            });
+
+            return View(lista);
+        }
+    }
+}

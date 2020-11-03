0a4d26a759319524e2f49d7e9c936488fe0e3415
heitor-bittencourt <heitor.g.bittencourt@gmail.com>
2020-11-02 23:40:35 -0300

    Vendedor Service e FindAll Síncrono

---
+++ WebVendasMvc/Services/VendedorService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebVendasMvc.Models;
+
+namespace WebVendasMvc.Services
+{
+    public class VendedorService
+    {
+        private readonly WebVendasMvcContext _context;
+
+        public VendedorService(WebVendasMvcContext context)
+        {
+            _context = context;
+        }
+
+        public List<Vendedor> FindAll()
+        {
+            return _context.Vendedor.ToList();
+        }
+    }
+}
--- WebVendasMvc/Startup.cs
+++ WebVendasMvc/Startup.cs
@@ -12,6 +12,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.EntityFrameworkCore;
 using WebVendasMvc.Models;
 using WebVendasMvc.Data;
+using WebVendasMvc.Services;
 
 namespace WebVendasMvc
 {
@@ -42,6 +43,7 @@ namespace WebVendasMvc
                     builder.MigrationsAssembly("WebVendasMvc")));
 
             services.AddScoped<SeedingService>();
+            services.AddScoped<VendedorService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.

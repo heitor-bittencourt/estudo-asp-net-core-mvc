c5e7883b9cd11031daf38d3de8c02762aeb549bc
heitor-bittencourt <heitor.g.bittencourt@gmail.com>
2020-11-11 06:25:55 -0300

    Localização e formatação de números e datas

--- WebVendasMvc/Models/Vendedor.cs
+++ WebVendasMvc/Models/Vendedor.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,9 +9,21 @@ namespace WebVendasMvc.Models
     public class Vendedor
     {
         public int Id { get; set; }
+        [Display(Name ="Nome")]
         public string Name { get; set; }
+
+        [DataType(DataType.EmailAddress)]
         public string Email { get; set; }
+
+        [Display(Name ="Data de Nascimento")]
+        [DataType(DataType.Date)]
+        /*Não é necessário utilizar esse display nesse formato, pois estamos utilizando a localização pt-BR
+         * [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}")]
+         */
         public DateTime DataAniversario { get; set; }
+
+        [Display(Name ="Salário Base")]
+        [DisplayFormat(DataFormatString ="{0:F2}")]
         public double SalarioBase { get; set; }
         public Departamento Departamento { get; set; }
         public int DepartamentoId { get; set; }
